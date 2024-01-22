fdc4eafa455f0cb730aea113b91f21fa2a566e5f
i'm betmann <143951155+vitorbetmann@users.noreply.github.com>
2024-01-21 19:54:45 -0500

    Update main.py

--- day006 ~Escaping The Maze~/main.py
+++ day006 ~Escaping The Maze~/main.py
@@ -1,4 +1,5 @@
-#solution for the maze challenge at :https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+#solution for the maze challenge at :
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 
 def turn_right():
     for i in range(3):
